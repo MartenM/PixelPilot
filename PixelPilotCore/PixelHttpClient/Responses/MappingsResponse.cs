7e6fead13543aa44ad409e2d8d1236199b261e36
MartenM <marten.struijk@gmail.com>
2024-04-28 12:23:36 +0200

    Include a method to generate C# enum like strings.

--- PixelPilotCore/PixelHttpClient/Responses/MappingsResponse.cs
+++ PixelPilotCore/PixelHttpClient/Responses/MappingsResponse.cs
@@ -1,11 +1,30 @@
-﻿namespace PixelPilot.PixelHttpClient.Responses;
+﻿using System.Globalization;
+
+namespace PixelPilot.PixelHttpClient.Responses;
 
 public class MappingsResponse
 {
-    private Dictionary<String, int> mappings { get; }
+    public Dictionary<String, int> mappings { get; }
 
     public MappingsResponse(Dictionary<string, int> mappings)
     {
         this.mappings = mappings;
+    }
+
+    /// <summary>
+    /// Method used to quickly generate a ENUM like mapping from the entries.
+    /// </summary>
+    /// <returns></returns>
+    public List<string> AsEnumEntries()
+    {
+        var list = new List<string>();
+        var textInfo = new CultureInfo("en-US", false).TextInfo;
+        foreach (var pair in mappings)
+        {
+            var words = pair.Key.Split("_");
+            list.Add($"{String.Join(null, words.Select(w => textInfo.ToTitleCase(w)))} = {pair.Value}");
+        }
+
+        return list;
     }
 }
