d2503716c9b262877821c691115abf5560c4e00b
MartenM <marten.struijk@gmail.com>
2024-04-28 14:33:28 +0200

    Updated example

--- PixelPilotExample/Program.cs
+++ PixelPilotExample/Program.cs
@@ -3,6 +3,8 @@ using PixelPilot.Models;
 using PixelPilot.PixelGameClient;
 using PixelPilot.PixelGameClient.Messages.Received;
 using PixelPilot.PixelGameClient.Messages.Send;
+using PixelPilot.PixelGameClient.World;
+using PixelPilot.PixelGameClient.World.Constants;
 using PixelPilotExample;
 
 // Load the configuration. Don't store your account token in the code :)
@@ -19,6 +21,18 @@ if (config == null)
 
 // Create a client.
 var client = new PixelPilotClient(config.AccountToken, false);
+
+// Create a PixelWorld class and attach the client to it.
+// Allow it to listen to client updates.
+var world = new PixelWorld();
+client.OnPacketReceived += world.HandlePacket;
+world.OnBlockPlaced += (_, playerId, oldBlock, newBlock) =>
+{
+    if (client.BotId == playerId) return;
+    //client.Send(new PlayerChatOutPacket($"A {newBlock.Block} was placed by user with ID 1!"));
+    client.Send(oldBlock.AsPacketOut());
+};
+
 await client.Connect(RoomType.Pixelwalker3, "r082b210d67df52");
 
 
@@ -34,7 +48,7 @@ client.OnPacketReceived += (_, packet) =>
             Environment.Exit(0);
             return;
         }
-        if (chat.Message.Equals(".ping")) client.Send(new PlayerChatOutPacket("Pong!"));
+        if (chat.Message.Equals(".ping")) client.Send(new PlayerChatOutPacket($"Pong! ({chat.Id})"));
     }
 };
 
@@ -43,6 +57,7 @@ client.OnPacketReceived += (_, packet) =>
 client.OnClientConnected += (_) =>
 {
     client.Send(new PlayerChatOutPacket("Hello world using the PixelPilot API."));
+    Thread.Sleep(250);
     PlatformUtil.GetThread(client).Start();
     client.Send(new PlayerMoveOutPacket(592, 1056, 0, 0, 0, 0, 0, 0, false, false, 100)); 
 };
