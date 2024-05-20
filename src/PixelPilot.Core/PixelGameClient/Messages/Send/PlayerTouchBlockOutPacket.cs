9efed7805c70735e14527be3b0d90a66c0e2713d
MartenM <marten.struijk@gmail.com>
2024-05-20 14:26:24 +0200

    Added PlayerTouchBlockOutPacket.cs

---
+++ src/PixelPilot.Core/PixelGameClient/Messages/Send/PlayerTouchBlockOutPacket.cs
@@ -0,0 +1,12 @@
+﻿using PixelPilot.PixelGameClient.Messages.Constants;
+
+namespace PixelPilot.PixelGameClient.Messages.Send;
+
+public class PlayerTouchBlockOutPacket : ReflectivePixelOutPacket
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int BlockId { get; set; }
+    
+    public PlayerTouchBlockOutPacket() : base(WorldMessageType.PlayerTouchBlock) {}
+}
