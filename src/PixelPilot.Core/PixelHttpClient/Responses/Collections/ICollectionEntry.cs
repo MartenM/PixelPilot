c7981ea7699b2a8a094d066a739a2e7cbe44b6d0
MartenM <marten.struijk@gmail.com>
2024-05-21 18:09:15 +0200

    Basic collection

---
+++ src/PixelPilot.Core/PixelHttpClient/Responses/Collections/ICollectionEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace PixelPilot.PixelHttpClient.Responses.Collections;
+
+public class ICollectionEntry
+{
+    public string CollectionId { get; }
+    public string CollectionName { get; }
+    public string Created { get; }
+}
