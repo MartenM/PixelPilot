9f54bcbdf191ad479098967ae3ec347ff1ebb4f8
MartenM <marten.struijk@gmail.com>
2024-05-29 11:55:41 +0200

    Extra world extensions

--- src/PixelPilot.Structures/Extensions/WorldExtensions.cs
+++ src/PixelPilot.Structures/Extensions/WorldExtensions.cs
@@ -15,9 +15,9 @@ public static class WorldExtensions
         // Copy the world based on X Y
         for (int layer = 1; layer >= 0; layer--)
         {
-            for (int dx = 0; dx <= width; dx++)
+            for (int dx = 0; dx < width; dx++)
             {
-                for (int dy = 0; dy <= height; dy++)
+                for (int dy = 0; dy < height; dy++)
                 {
                     var block = world.BlockAt(layer, x + dx, y + dy);
                     if (block.Block == PixelBlock.Empty && !copyEmpty) continue;
@@ -37,15 +37,57 @@ public static class WorldExtensions
         var width = Math.Abs(p1.X - p2.X);
         var height = Math.Abs(p1.Y - p2.Y);
 
-        return GetStructure(world, topleft.X, topleft.Y, width, height, copyEmpty);
+        return GetStructure(world, topleft.X, topleft.Y, width + 1, height + 1, copyEmpty);
     }
 
-    public static async Task PasteInOrder(this Structure structure, PixelPilotClient client, Point origin, int delay)
+    public static List<IPlacedBlock> GetDifference(this PixelWorld world, Structure structure, int x = 0, int y = 0)
     {
-        foreach (var block in structure.Blocks)
+        List<IPlacedBlock> difference = new();
+        if (structure.Width + x > world.Width || structure.Height + y > world.Height)
+            throw new PixelGameException(
+                "Attempted to a get the difference between a structure and world, but the structure goes outside the world!");
+        
+        foreach (var block in structure.BlocksWithEmpty)
+        {
+            var worldBlock = world.BlockAt(block.Layer, block.X + x, block.Y + x);
+            if (block.Block.Equals(worldBlock)) continue;
+
+            difference.Add(block);
+        }
+
+        return difference;
+    }
+    public static async Task PasteInOrder(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay)
+    {
+        foreach (var block in blocks)
         {
             client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
             await Task.Delay(delay);
+        }
+    }
+    
+    public static async Task PasteShuffled(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay)
+    {
+        var shuffeled = new List<IPlacedBlock>(blocks);
+        Shuffle(shuffeled);
+        foreach (var block in shuffeled)
+        {
+            client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
+            await Task.Delay(delay);
+        }
+    }
+    
+    private static void Shuffle<T>(this IList<T> list)  
+    {  
+        var rng = new Random();
+        var n = list.Count;
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
         }
     }
 }
