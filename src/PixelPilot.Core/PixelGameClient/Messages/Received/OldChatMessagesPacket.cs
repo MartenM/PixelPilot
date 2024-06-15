a159ef5ac4d26ec256fa79bdd773a25212ea664b
MartenM <marten.struijk@gmail.com>
2024-06-15 12:41:47 +0200

    Added OldChatMessagesPacket.cs

---
+++ src/PixelPilot.Core/PixelGameClient/Messages/Received/OldChatMessagesPacket.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace PixelPilot.PixelGameClient.Messages.Received;
+
+public class OldChatMessagesPacket : IPixelGamePacket
+{
+    public OldChatMessagesPacket(string username, string message, int color)
+    {
+        Username = username;
+        Message = message;
+        ChatColor = Color.FromArgb(color);
+    }
+
+    public string Username { get; set; }
+    public string Message { get; set; }
+    public Color ChatColor { get; set; }
+}
