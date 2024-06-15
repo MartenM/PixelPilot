914f319886fbb0312202adb4039b2bbb08c50139
MartenM <marten.struijk@gmail.com>
2024-06-15 13:20:08 +0200

    Updated tests

--- src/PixelPilot.Tests/PacketOutTests.cs
+++ src/PixelPilot.Tests/PacketOutTests.cs
@@ -22,7 +22,7 @@ public class PacketOutTests
     [Test]
     public void PacketFaceOutTest()
     {
-        string faceChange = "6b-0c-03-00-00-00-02".ToUpper();
+        string faceChange = "6b-0d-03-00-00-00-02".ToUpper();
         
         var packet = new PlayerFaceOutPacket(2);
         Assert.That(BitConverter.ToString(packet.ToBinaryPacket()), Is.EqualTo(faceChange), "Packets should be constructed properly.");
@@ -44,7 +44,7 @@ public class PacketOutTests
     public void PacketPlayerMoveOutTest()
     {
         string move =
-            "6b-0a-06-40-82-80-00-00-00-00-00-06-40-91-40-00-00-00-00-00-06-00-00-00-00-00-00-00-00-06-c0-4a-00-00-00-00-00-00-06-00-00-00-00-00-00-00-00-06-40-00-00-00-00-00-00-00-03-00-00-00-00-03-00-00-00-00-07-01-07-01-03-00-00-04-e1".ToUpper();
+            "6b-0b-06-40-82-80-00-00-00-00-00-06-40-91-40-00-00-00-00-00-06-00-00-00-00-00-00-00-00-06-c0-4a-00-00-00-00-00-00-06-00-00-00-00-00-00-00-00-06-40-00-00-00-00-00-00-00-03-00-00-00-00-03-00-00-00-00-07-01-07-01-03-00-00-04-e1".ToUpper();
         
         var movePacket = new PlayerMoveOutPacket(592, 1104, 0, -52, 0, 2, 0, 0, true, true, 1249);
         Assert.That(BitConverter.ToString(movePacket.ToBinaryPacket()), Is.EqualTo(move), "Packets should be constructed properly.");
