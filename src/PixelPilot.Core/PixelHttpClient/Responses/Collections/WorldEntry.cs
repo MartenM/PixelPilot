11b17e71007fa62d16cf7d6fb8a7c91175f510e6
MartenM <marten.struijk@gmail.com>
2024-06-20 17:40:34 +0200

    Added some minor details

--- src/PixelPilot.Core/PixelHttpClient/Responses/Collections/WorldEntry.cs
+++ src/PixelPilot.Core/PixelHttpClient/Responses/Collections/WorldEntry.cs
@@ -4,14 +4,20 @@ public class WorldEntry : ICollectionEntry
 {
     public string CollectionId { get; set; } = null!;
     public string CollectionName { get; set; } = null!;
-    public string Created { get; set; } = null!;
-    public string Data { get; set; } = null!;
+    
+    // In all
     public int Height { get; set; }
     public int Width { get; set; }
     public string Owner { get; set; } = null!;
+    public string Minimap { get; set; } = null!;
     public string Id { get; set; } = null!;
     public int Plays { get; set; }
     public string Title { get; set; } = null!;
+    
+    
+    // In collection worlds
     public string Updated { get; set; } = null!;
     public string Visibility { get; set; } = null!;
+    public string Created { get; set; } = null!;
+    public string Data { get; set; } = null!;
 }
