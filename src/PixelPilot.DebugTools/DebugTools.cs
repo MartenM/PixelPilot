b7539ca74b2ffab7d2050ff4286133cdc0fb4051
MartenM <marten.struijk@gmail.com>
2024-07-18 19:25:13 +0200

    Updated debug tools

--- src/PixelPilot.DebugTools/DebugTools.cs
+++ src/PixelPilot.DebugTools/DebugTools.cs
@@ -8,8 +8,8 @@ public class DebugTools
 {
     public class Options
     {
-        [Option('t', "tool", Required = true, HelpText = "The debug tool you want to use.")]
-        public string Tool { get; set; } = null!;
+        [Option('t', "tool", HelpText = "The debug tool you want to use.")]
+        public string? Tool { get; set; } = null!;
         
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
@@ -31,11 +31,18 @@ public class DebugTools
             {"json-packet-out", new JsonToPacket()}
         };
 
-        if (!tools.TryGetValue(options.Tool, out var tool))
+        if (options.Tool == null)
+        {
+            Console.Write("Please pick a tool: ");
+            options.Tool = Console.ReadLine();
+        }
+        
+        if (options.Tool == null || !tools.TryGetValue(options.Tool, out var tool))
         {
             Console.WriteLine("The specified tool could not be found.");
             return;
         }
+        
         
         Console.WriteLine($"Starting {options.Tool} tool...");
         tool.Execute();
