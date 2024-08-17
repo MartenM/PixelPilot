b02031184bc4cc64e17e5ee7cad30448d61523b7
MartenM <marten.struijk@gmail.com>
2024-08-17 14:16:37 +0200

    Added magic server value

--- src/PixelPilot.Core/PixelGameClient/Messages/Received/PlayerResetEffectsPacket.cs
+++ src/PixelPilot.Core/PixelGameClient/Messages/Received/PlayerResetEffectsPacket.cs
@@ -2,10 +2,13 @@
 
 public class PlayerResetEffectsPacket : IPixelGamePlayerPacket
 {
-    public PlayerResetEffectsPacket(int id)
+    public PlayerResetEffectsPacket(int id, bool byServer)
     {
         PlayerId = id;
+        ByServer = byServer;
     }
 
     public int PlayerId { get; }
+    
+    public bool ByServer { get; }
 }
