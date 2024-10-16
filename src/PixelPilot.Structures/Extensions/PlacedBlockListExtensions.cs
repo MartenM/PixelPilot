b1aba877e3e33b7909e69fa7a04146f09b95b93f
MartenM <marten.struijk@gmail.com>
2024-10-16 20:54:36 +0200

    BlockList modifications moved to their own place & fixed bug with ToChunkyPackets()

---
+++ src/PixelPilot.Structures/Extensions/PlacedBlockListExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+using PixelPilot.PixelGameClient;
+using PixelPilot.PixelGameClient.Messages;
+using PixelPilot.PixelGameClient.World.Blocks.Placed;
+using PixelPilot.PixelHttpClient;
+
+namespace PixelPilot.Structures.Extensions;
+
+public static class PlacedBlockListExtensions
+{
+    private const int MaxChunkBlockCount = 250;
+    
+    /// <summary>
+    /// Group blocks that are the same into a single packet that can be send.
+    /// </summary>
+    /// <param name="blocks">The blocks</param>
+    /// <returns>Packets to be send by the client.</returns>
+    public static List<IPixelGamePacketOut> ToChunkedPackets(this IEnumerable<IPlacedBlock> blocks)
+    {
+        var result = new List<IPixelGamePacketOut>();
+        
+        // Group the blocks per type/id
+        var groupedBlocks = blocks.GroupBy(block => new {Block = block.Block, Layer = block.Layer});
+        
+        // Now create the chunks of 250 blocks.
+        foreach (var rawGroup in groupedBlocks)
+        {
+            var typeBlocks = rawGroup.ToList();
+            for (int i = 0; i < typeBlocks.Count; i += MaxChunkBlockCount)
+            {
+                var chunk = typeBlocks.Skip(i).Take(MaxChunkBlockCount).ToList();
+                result.Add(chunk.ToChunkedPacket());
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Creates a packet out of a blocks that are all the same.
+    /// </summary>
+    /// <param name="blocks">The blocks</param>
+    /// <returns>A packet</returns>
+    /// <exception cref="PixelApiException">When the requirements are not met</exception>
+    public static IPixelGamePacketOut ToChunkedPacket(this List<IPlacedBlock> blocks)
+    {
+        var blockData = blocks.First().Block;
+        var layer = blocks.First().Layer;
+        
+        if (blocks.Count > MaxChunkBlockCount)
+            throw new PixelApiException("Cannot convert more than 250 blocks into a chunk.");
+
+        if (!blocks.All(block => block.Block.Equals(blockData)))
+            throw new PixelApiException("All blocks need to be the same for them to be chunked together.");
+
+        var positions = blocks.Select(b => new Point(b.X, b.Y)).ToList();
+        return blockData.AsPacketOut(positions, layer);
+    }
+    
+    public static async Task PasteInOrder(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay = 0)
+    {
+        foreach (var block in blocks)
+        {
+            client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
+            if (delay > 0) await Task.Delay(delay);
+        }
+    }
+    
+    public static async Task PasteShuffled(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay = 0)
+    {
+        var shuffeled = new List<IPlacedBlock>(blocks);
+        Shuffle(shuffeled);
+        foreach (var block in shuffeled)
+        {
+            client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
+            if (delay > 0) await Task.Delay(delay);
+        }
+    }
+    
+    private static void Shuffle<T>(this IList<T> list)  
+    {  
+        var rng = new Random();
+        var n = list.Count;
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
+}
--- src/PixelPilot.Structures/Extensions/WorldExtensions.cs
+++ src/PixelPilot.Structures/Extensions/WorldExtensions.cs
@@ -1,18 +1,24 @@
 ﻿using System.Drawing;
 using PixelPilot.PixelGameClient;
-using PixelPilot.PixelGameClient.Messages;
 using PixelPilot.PixelGameClient.World;
 using PixelPilot.PixelGameClient.World.Blocks;
 using PixelPilot.PixelGameClient.World.Blocks.Placed;
 using PixelPilot.PixelGameClient.World.Constants;
-using PixelPilot.PixelHttpClient;
 
 namespace PixelPilot.Structures.Extensions;
 
 public static class WorldExtensions
 {
-    public const int MaxChunkBlockCount = 250;
-    
+    /// <summary>
+    /// Get a structure from the world.
+    /// </summary>
+    /// <param name="world"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <param name="copyEmpty"></param>
+    /// <returns></returns>
     public static Structure GetStructure(this PixelWorld world, int x, int y, int width, int height, bool copyEmpty = true)
     {
         List<IPlacedBlock> blocks = new();
@@ -34,6 +40,14 @@ public static class WorldExtensions
         return new Structure(width, height, new Dictionary<string, string>(), copyEmpty, blocks);
     }
     
+    /// <summary>
+    /// Get a structure from the world between two points.
+    /// </summary>
+    /// <param name="world"></param>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <param name="copyEmpty"></param>
+    /// <returns></returns>
     public static Structure GetStructure(this PixelWorld world, Point p1, Point p2, bool copyEmpty = false)
     {
         var topleft = new Point(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y));
@@ -79,83 +93,22 @@ public static class WorldExtensions
         var translated = difference.Select(pb => new PlacedBlock(pb.X + x, pb.Y + y, pb.Layer, (IPixelBlock) pb.Block.Clone())).ToList();
         return translated.Cast<IPlacedBlock>().ToList();
     }
-
-    /// <summary>
-    /// Group blocks that are the same into a single packet that can be send.
-    /// </summary>
-    /// <param name="blocks">The blocks</param>
-    /// <returns>Packets to be send by the client.</returns>
-    public static List<IPixelGamePacketOut> ToChunkedPackets(this IEnumerable<IPlacedBlock> blocks)
-    {
-        var result = new List<IPixelGamePacketOut>();
-        
-        // Group the blocks per type/id
-        var groupedBlocks = blocks.GroupBy(block => block.Block);
-        
-        // Now create the chunks of 250 blocks.
-        foreach (var rawGroup in groupedBlocks)
-        {
-            var typeBlocks = rawGroup.ToList();
-            for (int i = 0; i < typeBlocks.Count; i += MaxChunkBlockCount)
-            {
-                var chunk = typeBlocks.Skip(i).Take(MaxChunkBlockCount).ToList();
-                result.Add(chunk.ToChunkedPacket());
-            }
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// Creates a packet out of a blocks that are all the same.
-    /// </summary>
-    /// <param name="blocks">The blocks</param>
-    /// <returns>A packet</returns>
-    /// <exception cref="PixelApiException">When the requirements are not met</exception>
-    public static IPixelGamePacketOut ToChunkedPacket(this List<IPlacedBlock> blocks)
-    {
-        var blockData = blocks.First().Block;
-        var layer = blocks.First().Layer;
-        
-        if (blocks.Count > MaxChunkBlockCount)
-            throw new PixelApiException("Cannot convert more than 250 blocks into a chunk.");
-
-        if (!blocks.All(block => block.Block.Equals(blockData)))
-            throw new PixelApiException("All blocks need to be the same for them to be chunked together.");
-
-        var positions = blocks.Select(b => new Point(b.X, b.Y)).ToList();
-        return blockData.AsPacketOut(positions, layer);
-    }
     
-    public static async Task PasteInOrder(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay = 0)
+    public static async Task PasteSafe(this PixelWorld world, Structure structure, PixelPilotClient client, Point pasteLocation, int maxAttempts = 5)
     {
-        foreach (var block in blocks)
+        int attempts = 0;
+        while (attempts < maxAttempts)
         {
-            client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
-            if (delay > 0) await Task.Delay(delay);
+            var diff = world.GetDifference(structure, pasteLocation.X, pasteLocation.Y, true);
+            if (diff.Count == 0) return;
+            
+            // Send packets. Assumes max ping 250.
+            client.SendRange(diff.ToChunkedPackets());
+            await Task.Delay(250 + diff.Count * 5);
+            
+            attempts++;
         }
-    }
-    
-    public static async Task PasteShuffled(this List<IPlacedBlock> blocks, PixelPilotClient client, Point origin, int delay = 0)
-    {
-        var shuffeled = new List<IPlacedBlock>(blocks);
-        Shuffle(shuffeled);
-        foreach (var block in shuffeled)
-        {
-            client.Send(block.Block.AsPacketOut(block.X + origin.X, block.Y + origin.Y, block.Layer));
-            if (delay > 0) await Task.Delay(delay);
-        }
-    }
-    
-    private static void Shuffle<T>(this IList<T> list)  
-    {  
-        var rng = new Random();
-        var n = list.Count;
-
-        for (int i = n - 1; i > 0; i--)
-        {
-            int j = rng.Next(i + 1);
-            (list[i], list[j]) = (list[j], list[i]);
-        }
+        
+        if (attempts >= maxAttempts) throw new Exception("Too many attempts to paste the structure!");
     }
 }
