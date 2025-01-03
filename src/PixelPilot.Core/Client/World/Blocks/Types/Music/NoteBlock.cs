d7a7466ec65581b5c1917c98e7d36801d1b3c407
MartenM <marten.struijk@gmail.com>
2025-01-03 09:36:18 +0100

    Fixed NoteBlock.cs Equals

--- src/PixelPilot.Core/Client/World/Blocks/Types/Music/NoteBlock.cs
+++ src/PixelPilot.Core/Client/World/Blocks/Types/Music/NoteBlock.cs
@@ -55,7 +55,7 @@ public class NoteBlock : BasicBlock
         if (ReferenceEquals(null, obj)) return false;
         if (ReferenceEquals(this, obj)) return true;
         if (obj.GetType() != this.GetType()) return false;
-        return Equals((ResetterBlock)obj);
+        return Equals((NoteBlock)obj);
     }
 
     public override int GetHashCode()
