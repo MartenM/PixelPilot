42148c76629af85f473544f9c58f1e15350dc4bd
Marten Struijk <marten.struijk@gmail.com>
2025-08-19 17:44:26 +0200

    Added structure loading tests.

--- src/PixelPilot.Tests/Structures/PixelPilotStuctureTests.cs
+++ src/PixelPilot.Tests/Structures/PixelPilotStuctureTests.cs
@@ -1,4 +1,5 @@
 ﻿using PixelPilot.Client;
+using PixelPilot.Client.World;
 using PixelPilot.Structures;
 using PixelPilot.Structures.Converters.PilotSimple;
 using PixelPilot.Structures.Extensions;
@@ -8,7 +9,27 @@ namespace PixelGameTests.Structures;
 public class PixelPilotStuctureTests
 {
     private PixelPilotClient _client;
-    private static Random _random = new Random();
+    private static Random _random = new();
+
+    private static List<string> TestPopulatedWorlds =
+    [
+        "r082b210d67df52",
+        "yzqm9vra06rqo80",
+        "8xkpxqn1hj2danj",
+        "legacy:PWAFxTxHVxa0I",
+        "legacy:PWhhGENdqja0I"
+    ];
+    
+    // Datasource for a list of stucture files.
+    private static readonly string TestStructuresFolder = "Structures/TestFiles";
+    public static IEnumerable<string> TestStructureFiles()
+    {
+        string folder = Path.Combine(TestContext.CurrentContext.TestDirectory, TestStructuresFolder);
+        foreach (var file in Directory.GetFiles(folder, "*.json"))
+        {
+            yield return Path.GetFileName(file);
+        }
+    }
     
     [SetUp]
     public void Setup()
@@ -35,24 +56,15 @@ public class PixelPilotStuctureTests
             .SetAutomaticReconnect(false)
             .Build();
     }
-    
-    // This will be your data source: one entry per file
-    public static IEnumerable<string> TestStructureFiles()
-    {
-        string folder = Path.Combine(TestContext.CurrentContext.TestDirectory, "Structures/TestFiles");
-        foreach (var file in Directory.GetFiles(folder, "*.json"))
-        {
-            yield return file;
-        }
-    }
 
     [TestCaseSource(nameof(TestStructureFiles))]
     [Parallelizable(ParallelScope.All)]
-    public async Task TestStuctureLoading(string filePath)
+    public async Task TestStuctureLoading(string fileName)
     {
-        // Arrange
+        // Load file contents
+        var filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, TestStructuresFolder, fileName);
         var contents = await File.ReadAllTextAsync(filePath);
-        Assert.That(contents, Is.Not.Empty, $"File {Path.GetFileName(filePath)} should not be empty.");
+        Assert.That(contents, Is.Not.Empty, $"File {fileName} should not be empty.");
 
         // Should go without errors.
         var structure = PilotSaveSerializer.Deserialize(contents);
@@ -62,9 +74,10 @@ public class PixelPilotStuctureTests
 
     [TestCaseSource(nameof(TestStructureFiles))]
     [Timeout(600000)]
-    public async Task TestStucturePasting(string filePath)
+    public async Task TestStucturePasting(string fileName)
     {
-        // Arrange
+        // Load file contents
+        var filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, TestStructuresFolder, fileName);
         var contents = await File.ReadAllTextAsync(filePath);
         var structure = PilotSaveSerializer.Deserialize(contents);
         
@@ -86,7 +99,32 @@ public class PixelPilotStuctureTests
         _client.SendRange(blockMessages);
         await _client.WaitForEmptyQueue();
         
-        Assert.That(blockMessages, Is.Not.Empty, $"Client should not have disconnected.");
+        Assert.Multiple(() =>
+        {
+            Assert.That(blockMessages, Is.Not.Empty, $"Safety check: At least placed some blocks.");
+            Assert.That(_client.IsConnected, Is.True, $"Client should not have disconnected.");
+            Assert.That(_client.LastException, Is.Null, $"Client should have not generated any issues.");
+        });
+        
+        await _client.Disconnect();
+    }
+
+    [TestCaseSource(nameof(TestPopulatedWorlds))]
+    [Timeout(600000)]
+    public async Task TestWorldParsing(string worldId)
+    {
+        var world = new PixelWorld();
+        _client.OnPacketReceived += world.HandlePacket;
+        
+        await _client.Connect(worldId);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(_client.LastException, Is.Null, $"Exception thrown during connecting / world loading. {_client.LastException}");
+            Assert.That(_client.IsConnected, Is.True, $"Required: Client should be connected.");
+        });
+        
+        await Task.Delay(1000);
         await _client.Disconnect();
     }
 }
