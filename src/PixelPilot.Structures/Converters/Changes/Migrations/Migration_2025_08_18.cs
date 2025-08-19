f8232bb05edae056aa85ccd4b1bec310e2dbf218
Marten Struijk <marten.struijk@gmail.com>
2025-08-19 17:43:43 +0200

    Updated Migration_2025_08_18.cs

--- src/PixelPilot.Structures/Converters/Changes/Migrations/Migration_2025_08_18.cs
+++ src/PixelPilot.Structures/Converters/Changes/Migrations/Migration_2025_08_18.cs
@@ -7,13 +7,55 @@ public class Migration_2025_08_18() : VersionMigration(9)
 {
     protected override void DoUpdate(MappedBlockData mappedBlockData)
     {
+        int tempSpaceSignId = -1;
         for (int i = 0; i < mappedBlockData.Mapping.Count; i++)
         {
             var entry = mappedBlockData.Mapping[i];
             if (entry.Equals("OuterspaceSign"))
             {
                 mappedBlockData.Mapping[i] = "OuterspaceSignGreen";
+                tempSpaceSignId = i;
+                break;
             }
         }
+
+        if (tempSpaceSignId == -1)
+        {
+            return;
+        }
+
+        // Go over all entries and modify where required.
+        for (int i = mappedBlockData.BlockData.Count - 1; i >= 0; i--)
+        {
+            using var bytes = new MemoryStream(Convert.FromBase64String(mappedBlockData.BlockData[i]));
+            using var binReader = new BinaryReader(bytes);
+
+            var x = binReader.ReadInt32();
+            var y = binReader.ReadInt32();
+            var layer = binReader.ReadInt32();
+
+            var tempId = binReader.ReadInt32();
+
+            // If this is a portal block.
+            if (tempId != tempSpaceSignId) continue;
+
+            var migratedBlock = Convert.ToBase64String(CreateSignBlock(x, y, layer, tempId, ""));
+            mappedBlockData.BlockData[i] = migratedBlock;
+        }
     }
+
+    private byte[] CreateSignBlock(int x, int y, int layer, int customId, string text)
+    {
+        using MemoryStream memoryStream = new MemoryStream();
+        using BinaryWriter writer = new BinaryWriter(memoryStream);
+
+        writer.Write(x);
+        writer.Write(y);
+        writer.Write(layer);
+        writer.Write(customId);
+        writer.Write(text);
+
+        return memoryStream.ToArray();
+    }
+
 }
