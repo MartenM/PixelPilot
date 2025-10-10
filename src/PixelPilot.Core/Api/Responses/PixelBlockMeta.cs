81332766557da7604329f3287c568f71919e7f2d
Marten Struijk <marten.struijk@gmail.com>
2025-10-10 09:42:03 +0200

    Updated API client to migrate from listblocks.

---
+++ src/PixelPilot.Core/Api/Responses/PixelBlockMeta.cs
@@ -0,0 +1,11 @@
+﻿namespace PixelPilot.Api.Responses;
+
+public class PixelBlockMeta
+{
+    public required int Id { get; init; }
+    public required string PaletteId { get; init; }
+    public required int Layer { get; init; }
+    
+    public long? MinimapColor { get; init; }
+    public int? LegacyId { get; init; }
+}
