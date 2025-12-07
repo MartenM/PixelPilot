8a74a6910f67df72db3cdd469289d4b2ca27b7a9
Marten Struijk <marten.struijk@gmail.com>
2025-12-07 11:06:55 +0100

    Endpoints configurable

--- src/PixelPilot.Core/Common/EndPoints.cs
+++ src/PixelPilot.Core/Common/EndPoints.cs
@@ -2,9 +2,62 @@
 
 public static class EndPoints
 {
-    public const string SiteUrl = "pw-staging.rnc.priddle.nl";
-    
-    public const string ApiEndpoint = $"https://api.{SiteUrl}";
-    public const string GameHttpEndpoint = $"https://server.{SiteUrl}";
-    public const string GameWebsocketEndpoint = $"wss://server.{SiteUrl}";
+    public const string StagingUrl = "pw-staging.rnc.priddle.nl";
+    public const string ProductionUrl = "pixelwalker.net";
+
+    public static string ApiEndpoint
+    {
+        get
+        {
+            if (UseStaging)
+            {
+                return $"https://api.{StagingUrl}";
+            }
+            return $"https://api.{ProductionUrl}";
+        }
+    }
+
+    public static string GameHttpEndpoint
+    {
+        get
+        {
+            if (UseStaging)
+            {
+                return $"https://server.{StagingUrl}";
+            }
+            return $"https://server.{ProductionUrl}";
+        }
+    }
+
+    public static string GameWebsocketEndpoint
+    {
+        get
+        {
+            if (UseStaging)
+            {
+                return $"wss://server.{StagingUrl}";
+            }
+
+            return $"wss://server.{ProductionUrl}";
+        }
+    }
+
+    public static bool UseStaging
+    {
+        get
+        {
+            var apiEnv = Environment.GetEnvironmentVariable("PIXELWALKER_ENDPOINT");
+            if (apiEnv == null)
+            {
+                return false;
+            }
+
+            if (apiEnv.ToLower() == "staging")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
 }
